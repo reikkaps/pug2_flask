a1bc667431361d38137767507cfd80329d4b1d7f
Reik Kaps <r31k@k4p5.de>
2016-10-17 22:02:10 +0200

    Etwas Feintuning ...

--- hello.py
+++ hello.py
@@ -1,17 +1,18 @@
 
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask
+#, render_template, url_for, request, flash, redirect
 
 app = Flask(__name__)
 
 ##
 # flask routing via decorators
 @app.route("/hello/")
+# can hold variable parts
 @app.route("/hello/<name>")
 def hello(name=None):
     if name != None:
         return 'Hello {}'.format(Name)
     return 'Hello World'
-
 
 @app.route("/")
 def index():
@@ -20,6 +21,7 @@ def index():
 ##
 # main for simple testing
 if __name__ == "__main__":
+    # http://stackoverflow.com/questions/22463939/demystify-flask-app-secret-key#22463969
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(debug=True, 
             port=8888,
